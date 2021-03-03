502ff5b935e210454aa4be7c3af293f374898c99
sorgelig <pour.garbage@gmail.com>
2021-03-03 17:42:21 +0800

    Update sys. HV-Integer scale.

--- Oric.sv
+++ Oric.sv
@@ -39,8 +39,9 @@ module emu
 	output        CE_PIXEL,
 
 	//Video aspect ratio for HDMI. Most retro systems have ratio 4:3.
-	output [11:0] VIDEO_ARX,
-	output [11:0] VIDEO_ARY,
+	//if VIDEO_ARX[12] or VIDEO_ARY[12] is set then [11:0] contains scaled size instead of aspect ratio.
+	output [12:0] VIDEO_ARX,
+	output [12:0] VIDEO_ARY,
 
 	output  [7:0] VGA_R,
 	output  [7:0] VGA_G,
@@ -51,6 +52,9 @@ module emu
 	output        VGA_F1,
 	output [1:0]  VGA_SL,
 	output        VGA_SCALER, // Force VGA scaler
+
+	input  [11:0] HDMI_WIDTH,
+	input  [11:0] HDMI_HEIGHT,
 
 `ifdef USE_FB
 	// Use framebuffer in DDRAM (USE_FB=1 in qsf)
@@ -185,9 +189,18 @@ assign AUDIO_S   = 0;
 assign AUDIO_MIX = 0;
 
 wire [1:0] ar = status[14:13];
+video_freak video_freak
+(
+	.*,
+	.VGA_DE_IN(VGA_DE),
+	.VGA_DE(),
 
-assign VIDEO_ARX = (!ar) ? 12'd4 : (ar - 1'd1);
-assign VIDEO_ARY = (!ar) ? 12'd3 : 12'd0;
+	.ARX((!ar) ? 12'd4 : (ar - 1'd1)),
+	.ARY((!ar) ? 12'd3 : 12'd0),
+	.CROP_SIZE(0),
+	.CROP_OFF(0),
+	.SCALE(status[16:15])
+);
 
 `include "build_id.v"
 localparam CONF_STR = {
@@ -200,6 +213,7 @@ localparam CONF_STR = {
 	"-;",
 	"ODE,Aspect ratio,Original,Full Screen,[ARC1],[ARC2];",
 	"OAC,Scandoubler Fx,None,HQ2x,CRT 25%,CRT 50%;",
+	"OFG,Scale,Normal,V-Integer,Narrower HV-Integer,Wider HV-Integer;",
 	"-;",
 	"O8,Stereo,Off,On;",
 	"-;",
@@ -398,13 +412,11 @@ reg clk_pix2;
 always @(posedge clk_sys) clk_pix2 <= clk_pix;
 
 reg ce_pix;
-reg ce_pix2;
 always @(posedge CLK_VIDEO) begin
 	reg old_clk;
 	
 	old_clk <= clk_pix2;
 	ce_pix <= ~old_clk & clk_pix2;
-	ce_pix2 <= old_clk ^ clk_pix2;
 end
 
 reg HSync, VSync;
@@ -422,21 +434,13 @@ wire       scandoubler = scale || forced_scandoubler;
 assign VGA_F1 = 0;
 assign VGA_SL = sl[1:0];
 
-assign CE_PIXEL = scandoubler ? ce_pix_out : ce_pix2;
-
-wire ce_pix_out;
 video_mixer #(.LINE_LENGTH(250), .HALF_DEPTH(1), .GAMMA(1)) video_mixer
 (
 	.*,
-	.clk_vid(CLK_VIDEO),
-	
 	.R({4{r}}),
 	.G({4{g}}),
 	.B({4{b}}),
-
-	.scanlines(0),
-	.hq2x(scale==1),
-	.mono(0)
+	.hq2x(scale==1)
 );
 
 ///////////////////////////////////////////////////
